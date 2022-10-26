1dfc65876195a9668d312eb3a6edca6c67c6a5ab
KurniawanDwiYuliyanto2057051010 <kurniawandwiyuliyanto42@gmail.com>
2022-10-26 22:36:04 +0700

    nambahview

--- app/Views/suratmasuk/list.php
+++ app/Views/suratmasuk/list.php
@@ -1,55 +1,94 @@
-<a href="/create" type="button" class="btn btn-primary mb-3">Tambah</a>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nomor Surat</th>
-      <th scope="col">Tanggal Terima Surat</th>
-      <th scope="col">Tanggal Surat</th>
-      <th scope="col">Asal Surat</th>
-      <th scope="col">Isi Surat</th>
-      <th scope="col">Keterangan Surat</th>
-      <th scope="col">Created_At</th>
-      <th scope="col">Updated_At</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Surat Masuk</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">DataTables</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">DataTable with minimal features & hover style</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <a href="/create" type="button" class="btn btn-primary mb-3">Tambah</a>
+              <table class="table table-bordered table-hover justify-text-center">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nomor Surat</th>
+                    <th scope="col">Tanggal Terima Surat</th>
+                    <th scope="col">Tanggal Surat</th>
+                    <th scope="col">Asal Surat</th>
+                    <th scope="col">Isi Surat</th>
+                    <th scope="col">Keterangan Surat</th>
+                    <th scope="col">Created_At</th>
+                    <th scope="col">Updated_At</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
 
-    $no = 1;
+                  $no = 1;
 
-    foreach ($suratmasuk as $srtmsk) :
+                  foreach ($suratmasuk as $srtmsk) :
 
-    ?>
+                  ?>
 
-      <tr>
-        <th scope="row"><?= $no ?></th>
-        <td><?= $srtmsk['no_surat'] ?></td>
-        <td><?= $srtmsk['tgl_terima_surat'] ?></td>
-        <td><?= $srtmsk['tgl_surat'] ?></td>
-        <td><?= $srtmsk['asal_surat'] ?></td>
-        <td><?= $srtmsk['isi_surat'] ?></td>
-        <td><?= $srtmsk['keterangan_surat'] ?></td>
-        <td><?= $srtmsk['created_at'] ?></td>
-        <td><?= $srtmsk['updated_at'] ?></td>
-        <td>
-          <div class="d-flex">
-            <a class="btn btn-warning mr-3" href="/edit/<?= $srtmsk['id'] ?>">Edit</a>
-            <form action="/delete/<?= $srtmsk['id'] ?>" method="post">
-              <input name="_method" value="DELETE" type="hidden">
-              <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
+                    <tr>
+                      <th scope="row"><?= $no ?></th>
+                      <td><?= $srtmsk['no_surat'] ?></td>
+                      <td><?= $srtmsk['tgl_terima_surat'] ?></td>
+                      <td><?= $srtmsk['tgl_surat'] ?></td>
+                      <td><?= $srtmsk['asal_surat'] ?></td>
+                      <td><?= $srtmsk['isi_surat'] ?></td>
+                      <td><?= $srtmsk['keterangan_surat'] ?></td>
+                      <td><?= $srtmsk['created_at'] ?></td>
+                      <td><?= $srtmsk['updated_at'] ?></td>
+                      <td>
+                        <div class="d-flex">
+                          <a class="btn btn-warning mr-3" href="/edit/<?= $srtmsk['id'] ?>">Edit</a>
+                          <form action="/delete/<?= $srtmsk['id'] ?>" method="post">
+                            <input name="_method" value="DELETE" type="hidden">
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                          </form>
+                        </div>
+                      </td>
+                    </tr>
+
+                  <?php
+
+                    $no++;
+                  endforeach
+
+                  ?>
+
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
           </div>
-        </td>
-      </tr>
-
-    <?php
-
-      $no++;
-    endforeach
-
-    ?>
-
-  </tbody>
-</table>
+          <!-- /.card -->
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
